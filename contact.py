3e9ec8a64df75e6cbe0d0b80fdd476753b3dee60
Daniel Amos Grenehed <daniel.grenehed97@gmail.com>
2021-12-06 19:56:50 +0100

    number validation

--- contact.py
+++ contact.py
@@ -1,7 +1,11 @@
+import re
+
 class Contact() :
     def __init__(self, name, number=""):
         self.name = name
         self.number = number
+        if not self.isValid():
+            self.validateNumber()
 
     def __eq__(self, other):
         if other == None: return False
@@ -15,3 +19,19 @@ class Contact() :
 
     def __str__(self):
         return "(" + self.name + ", " + self.number + ")"
+
+    def isValid(self):
+        res = re.search("^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$", self.number)
+        return res != None
+
+    def validateNumber(self):
+        res = ""
+        found = re.findall("[0-9 ()+.-]", self.number)
+        for s in found:
+            res += s
+        self.number = res
+
+if __name__ == "__main__" :
+    c = Contact("test", "v+12 +4a3.2 4-12")
+    print(c)
+    print(c.isValid())
